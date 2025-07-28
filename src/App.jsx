3cf06f3b3bba5983d5a6bd62bf98409daa47c27b
calistasalscpw <calistasalsa.cpw@gmail.com>
2025-07-28 14:05:12 +0700

    fix: signup page

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Signup from './pages/Signup.jsx';
 
 function App() {
   
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/auth/signup' element={<Signup/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
-export default App
+export default App;
